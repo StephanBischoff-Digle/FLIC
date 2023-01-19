f29e586e4a1e7d6a6cbe0d79ff77c3f34c626fc6
Stephan Bischoff <stephan.bischoff@digle.net>
2023-01-20 00:23:26 +0100

    refactor: we don't use the noise arguments

--- PlugLIC/algos/SimplexNoise.cpp
+++ PlugLIC/algos/SimplexNoise.cpp
@@ -18,8 +18,8 @@
  * @return largest integer value not greater than fp
  */
 static inline int32_t fastfloor(float fp) {
-  int32_t i = static_cast<int32_t>(fp);
-  return (fp < i) ? (i - 1) : (i);
+    int32_t i = static_cast<int32_t>(fp);
+    return (fp < i) ? (i - 1) : (i);
 }
 
 static const uint8_t perm[256] = {
@@ -48,10 +48,10 @@ static inline uint8_t hash(int32_t i) { return perm[static_cast<uint8_t>(i)]; }
  * @return gradient value
  */
 static float grad(int32_t hash, float x, float y) {
-  const int32_t h = hash & 0x3F;  // Convert low 3 bits of hash code
-  const float u = h < 4 ? x : y;  // into 8 simple gradient directions,
-  const float v = h < 4 ? y : x;
-  return ((h & 1) ? -u : u) + ((h & 2) ? -2.0f * v : 2.0f * v);  // and compute the dot product with (x,y).
+    const int32_t h = hash & 0x3F;  // Convert low 3 bits of hash code
+    const float u = h < 4 ? x : y;  // into 8 simple gradient directions,
+    const float v = h < 4 ? y : x;
+    return ((h & 1) ? -u : u) + ((h & 2) ? -2.0f * v : 2.0f * v);  // and compute the dot product with (x,y).
 }
 
 /**
@@ -64,80 +64,80 @@ static float grad(int32_t hash, float x, float y) {
  * coordinates.
  */
 float SimplexNoise::noise(float x, float y) {
-  float n0, n1, n2;  // Noise contributions from the three corners
+    float n0, n1, n2;  // Noise contributions from the three corners
 
-  // Skewing/Unskewing factors for 2D
-  static const float F2 = 0.366025403f;  // F2 = (sqrt(3) - 1) / 2
-  static const float G2 = 0.211324865f;  // G2 = (3 - sqrt(3)) / 6   = F2 / (1 + 2 * K)
+    // Skewing/Unskewing factors for 2D
+    static const float F2 = 0.366025403f;  // F2 = (sqrt(3) - 1) / 2
+    static const float G2 = 0.211324865f;  // G2 = (3 - sqrt(3)) / 6   = F2 / (1 + 2 * K)
 
-  // Skew the input space to determine which simplex cell we're in
-  const float s = (x + y) * F2;  // Hairy factor for 2D
-  const float xs = x + s;
-  const float ys = y + s;
-  const int32_t i = fastfloor(xs);
-  const int32_t j = fastfloor(ys);
+    // Skew the input space to determine which simplex cell we're in
+    const float s = (x + y) * F2;  // Hairy factor for 2D
+    const float xs = x + s;
+    const float ys = y + s;
+    const int32_t i = fastfloor(xs);
+    const int32_t j = fastfloor(ys);
 
-  // Unskew the cell origin back to (x,y) space
-  const float t = static_cast<float>(i + j) * G2;
-  const float X0 = i - t;
-  const float Y0 = j - t;
-  const float x0 = x - X0;  // The x,y distances from the cell origin
-  const float y0 = y - Y0;
+    // Unskew the cell origin back to (x,y) space
+    const float t = static_cast<float>(i + j) * G2;
+    const float X0 = i - t;
+    const float Y0 = j - t;
+    const float x0 = x - X0;  // The x,y distances from the cell origin
+    const float y0 = y - Y0;
 
-  // For the 2D case, the simplex shape is an equilateral triangle.
-  // Determine which simplex we are in.
-  int32_t i1,
-      j1;         // Offsets for second (middle) corner of simplex in (i,j) coords
-  if (x0 > y0) {  // lower triangle, XY order: (0,0)->(1,0)->(1,1)
-    i1 = 1;
-    j1 = 0;
-  } else {  // upper triangle, YX order: (0,0)->(0,1)->(1,1)
-    i1 = 0;
-    j1 = 1;
-  }
+    // For the 2D case, the simplex shape is an equilateral triangle.
+    // Determine which simplex we are in.
+    int32_t i1,
+        j1;         // Offsets for second (middle) corner of simplex in (i,j) coords
+    if (x0 > y0) {  // lower triangle, XY order: (0,0)->(1,0)->(1,1)
+        i1 = 1;
+        j1 = 0;
+    } else {  // upper triangle, YX order: (0,0)->(0,1)->(1,1)
+        i1 = 0;
+        j1 = 1;
+    }
 
-  // A step of (1,0) in (i,j) means a step of (1-c,-c) in (x,y), and
-  // a step of (0,1) in (i,j) means a step of (-c,1-c) in (x,y), where
-  // c = (3-sqrt(3))/6
+    // A step of (1,0) in (i,j) means a step of (1-c,-c) in (x,y), and
+    // a step of (0,1) in (i,j) means a step of (-c,1-c) in (x,y), where
+    // c = (3-sqrt(3))/6
 
-  const float x1 = x0 - i1 + G2;  // Offsets for middle corner in (x,y) unskewed coords
-  const float y1 = y0 - j1 + G2;
-  const float x2 = x0 - 1.0f + 2.0f * G2;  // Offsets for last corner in (x,y) unskewed coords
-  const float y2 = y0 - 1.0f + 2.0f * G2;
+    const float x1 = x0 - i1 + G2;  // Offsets for middle corner in (x,y) unskewed coords
+    const float y1 = y0 - j1 + G2;
+    const float x2 = x0 - 1.0f + 2.0f * G2;  // Offsets for last corner in (x,y) unskewed coords
+    const float y2 = y0 - 1.0f + 2.0f * G2;
 
-  // Work out the hashed gradient indices of the three simplex corners
-  const int gi0 = hash(i + hash(j));
-  const int gi1 = hash(i + i1 + hash(j + j1));
-  const int gi2 = hash(i + 1 + hash(j + 1));
+    // Work out the hashed gradient indices of the three simplex corners
+    const int gi0 = hash(i + hash(j));
+    const int gi1 = hash(i + i1 + hash(j + j1));
+    const int gi2 = hash(i + 1 + hash(j + 1));
 
-  // Calculate the contribution from the first corner
-  float t0 = 0.5f - x0 * x0 - y0 * y0;
-  if (t0 < 0.0f) {
-    n0 = 0.0f;
-  } else {
-    t0 *= t0;
-    n0 = t0 * t0 * grad(gi0, x0, y0);
-  }
+    // Calculate the contribution from the first corner
+    float t0 = 0.5f - x0 * x0 - y0 * y0;
+    if (t0 < 0.0f) {
+        n0 = 0.0f;
+    } else {
+        t0 *= t0;
+        n0 = t0 * t0 * grad(gi0, x0, y0);
+    }
 
-  // Calculate the contribution from the second corner
-  float t1 = 0.5f - x1 * x1 - y1 * y1;
-  if (t1 < 0.0f) {
-    n1 = 0.0f;
-  } else {
-    t1 *= t1;
-    n1 = t1 * t1 * grad(gi1, x1, y1);
-  }
+    // Calculate the contribution from the second corner
+    float t1 = 0.5f - x1 * x1 - y1 * y1;
+    if (t1 < 0.0f) {
+        n1 = 0.0f;
+    } else {
+        t1 *= t1;
+        n1 = t1 * t1 * grad(gi1, x1, y1);
+    }
 
-  // Calculate the contribution from the third corner
-  float t2 = 0.5f - x2 * x2 - y2 * y2;
-  if (t2 < 0.0f) {
-    n2 = 0.0f;
-  } else {
-    t2 *= t2;
-    n2 = t2 * t2 * grad(gi2, x2, y2);
-  }
+    // Calculate the contribution from the third corner
+    float t2 = 0.5f - x2 * x2 - y2 * y2;
+    if (t2 < 0.0f) {
+        n2 = 0.0f;
+    } else {
+        t2 *= t2;
+        n2 = t2 * t2 * grad(gi2, x2, y2);
+    }
 
-  // Add contributions from each corner to get the final noise value.
-  // The result is scaled to return values in the interval [-1,1].
-  return 45.23065f * (n0 + n1 + n2);
+    // Add contributions from each corner to get the final noise value.
+    // The result is scaled to return values in the interval [-1,1].
+    return 45.23065f * (n0 + n1 + n2);
 }
--- PlugLIC/algos/SimplexNoise.h
+++ PlugLIC/algos/SimplexNoise.h
@@ -3,34 +3,6 @@
 #include <cstddef>
 
 class SimplexNoise {
- public:
-  static float noise(float x, float y);
-
-  /**
-   * Constructor of to initialize a fractal noise summation
-   *
-   * @param[in] frequency    Frequency ("width") of the first octave of noise
-   * (default to 1.0)
-   * @param[in] amplitude    Amplitude ("height") of the first octave of noise
-   * (default to 1.0)
-   * @param[in] lacunarity   Lacunarity specifies the frequency multiplier
-   * between successive octaves (default to 2.0).
-   * @param[in] persistence  Persistence is the loss of amplitude between
-   * successive octaves (usually 1/lacunarity)
-   */
-  explicit SimplexNoise(float frequency = 1.0f, float amplitude = 1.0f, float lacunarity = 2.0f,
-                        float persistence = 0.5f)
-      : mFrequency(frequency), mAmplitude(amplitude), mLacunarity(lacunarity), mPersistence(persistence) {}
-
- private:
-  // Parameters of Fractional Brownian Motion (fBm) : sum of N "octaves" of
-  // noise
-  float mFrequency;    ///< Frequency ("width") of the first octave of noise
-                       ///< (default to 1.0)
-  float mAmplitude;    ///< Amplitude ("height") of the first octave of noise
-                       ///< (default to 1.0)
-  float mLacunarity;   ///< Lacunarity specifies the frequency multiplier between
-                       ///< successive octaves (default to 2.0).
-  float mPersistence;  ///< Persistence is the loss of amplitude between
-                       ///< successive octaves (usually 1/lacunarity)
+   public:
+    static float noise(float x, float y);
 };
